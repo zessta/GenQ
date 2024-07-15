870b10b95d02f1f68bf8e445ec59824a1f47bfaa
Arka <arkajit.banerjee@zessta.com>
2024-07-15 17:24:08 +0530

    Create IDependencyInjection.js

---
+++ javascript1by1/Incorrectjs/IDependencyInjection.js
@@ -0,0 +1,38 @@
+class Container { 
+ constructor() { 
+ this.services = new Map(); 
+ } 
+ register(name, definition, dependencies) { 
+ this.services.set(name, { definition, dependencies }); 
+ } 
+ resolve(name) { 
+ const target = this.services.get(name); 
+ if (!target) { 
+ throw new Error(`Service not found: ${name}`); 
+ } 
+ if (!target.instance) { 
+ const { definition, dependencies } = target; 
+ const resolvedDependencies = dependencies.map(dep => this.resolve(dep));  target.instance = new definition(...resolvedDependencies); 
+ } 
+ return target.instance; 
+ } 
+} 
+class Logger { 
+ log(message) { 
+ console.log(message); 
+ } 
+} 
+class UserService { 
+ constructor(logger) { 
+ this.logger = logger; 
+ } 
+ getUser() { 
+ this.logger.log('User fetched'); 
+ return { name: 'John Doe' }; 
+ } 
+} 
+const container = new Container(); 
+container.register('logger', Logger, []); 
+container.register('userService', UserService, ['logger']); 
+const userService = container.resolve('userService'); 
+console.log(userService.getUser()); // Output: User fetched { name: 'John Doe' } ``` 
