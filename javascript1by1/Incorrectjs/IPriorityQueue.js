d08c55ae3b755fee484d6ea37dbe33ca0c53b850
Arka <arkajit.banerjee@zessta.com>
2024-07-15 17:09:40 +0530

    Create IPriorityQueue.js

---
+++ javascript1by1/Incorrectjs/IPriorityQueue.js
@@ -0,0 +1,26 @@
+class PriorityQueue { 
+ constructor() { 
+ this.queue = []; 
+ } 
+ enqueue(item, priority) { 
+ this.queue.push({ item, priority }); 
+ this.queue.sort((a, b) => b.priority - a.priority); 
+ } 
+ dequeue() { 
+ return this.queue.shift().item; 
+ } 
+ peek() { 
+ return this.queue[0]?.item; 
+ } 
+ isEmpty() { 
+ return this.queue.length === 0; 
+ } 
+} 
+// Usage example 
+const pq = new PriorityQueue(); 
+pq.enqueue('low priority task', 1); 
+pq.enqueue('high priority task', 10); 
+pq.enqueue('medium priority task', 5); 
+console.log(pq.dequeue()); // high priority task 
+console.log(pq.dequeue()); // medium priority task 
+console.log(pq.dequeue()); // low priority task 
